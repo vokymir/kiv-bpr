83f865aa4ebe485910f0a2c24832d167f76a7421
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 19:02:25 +0100

    update configs to work

--- src/Sim_Config.hpp
+++ src/Sim_Config.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
+#include "Vis_Config.hpp"
+#include <variant>
 namespace ssoc {
+
+// Graph Generation Algorithm
+namespace gga_ {
+
+struct Square_Lattice_2D {
+  int width;
+  int height;
+  bool circular_on_x;
+  bool circular_on_y;
+};
+
+} // namespace gga_
+
+using Graph_Generation_Algorithm = std::variant<gga_::Square_Lattice_2D>;
 
 /* Holds configuration for the simulation. Must be serializable, to save
  * specific config for future reference. */
-struct Sim_Config {};
+struct Sim_Config {
+  struct Vis_Config visual;
+  Graph_Generation_Algorithm gga;
+};
 
 } // namespace ssoc
--- src/Vis_Config.hpp
+++ src/Vis_Config.hpp
@@ -1,9 +1,29 @@
 #pragma once
 
+#include <variant>
 namespace ssoc {
+
+// Graph Layout Algorithm
+namespace gla_ {
+
+enum FR2D_Accuracy {
+  High,
+  Low, // uses GRID variant of alg
+  Auto // use GRID for LARGE graphs
+};
+
+struct Fruchterman_Reingold_2D {
+  FR2D_Accuracy accuracy;
+};
+
+} // namespace gla_
+
+using Graph_Layout_Algorithm = std::variant<gla_::Fruchterman_Reingold_2D>;
 
 /* Holds configuration for the visualisation of one simulation. Must be
  * serializable, to save specific config for future reference. */
-struct Vis_Config {};
+struct Vis_Config {
+  Graph_Layout_Algorithm gla;
+};
 
 } // namespace ssoc
