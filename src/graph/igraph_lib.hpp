28c8ead9fd9b762a7f19e8e04db85f07945ffc6f
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 20:20:03 +0100

    igraph: polish

--- src/graph/igraph_lib.hpp
+++ src/graph/igraph_lib.hpp
@@ -2,13 +2,15 @@
 
 #include "igraph.h"
 #include <memory>
+// underscore to make obvious its custom namespace for igraph
 namespace ssoc::graph::igraph_ {
 
-// custor raii deleter to allow unique_ptr
+// custor raii functor deleter to allow unique_ptr to hold igraph
 struct igraph_Deleter {
   void operator()(igraph_t *g) const;
 };
 
+// shorthand for using unique_ptr with igraph
 using unique_ptr_ = std::unique_ptr<igraph_t, igraph_Deleter>;
 
 } // namespace ssoc::graph::igraph_
