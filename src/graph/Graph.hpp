27890bd27d856cdc76698447f8019b8ae4ff94a4
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 20:37:40 +0100

    graph copy/move + default initializers

--- src/graph/Graph.hpp
+++ src/graph/Graph.hpp
@@ -23,16 +23,18 @@ private:
    *  0 0 2 2 3 3 3   <- whose neighbour list is it
    * [1,2,1,0,1,0,3]
    * */
-  std::vector<int> adj_offsets_;
-  std::vector<int> adj_vertices_;
+  std::vector<int> adj_offsets_{};
+  std::vector<int> adj_vertices_{};
 
   /* Sand count for every node. It's vectorized for batch runs, every run
    * having it's own vector. */
-  std::vector<std::vector<int>> sand_height_;
+  std::vector<std::vector<int>> sand_height_{};
 
   /* Store the x,y coordinates in 2D UI layout. */
-  std::vector<std::pair<double, double>> layout_pos_;
+  std::vector<std::pair<double, double>> layout_pos_{};
 
+  // =====
+  // get/set
 public:
   int num_vertices() const { return num_vertices_; }
   void num_vertices(int count) { num_vertices_ = count; }
@@ -59,6 +61,15 @@ public:
   const std::vector<std::pair<double, double>> &layout_pos() const {
     return layout_pos_;
   }
+
+  // =====
+  // copy/move
+public:
+  Graph(const Graph &) = delete;
+  Graph &operator=(const Graph &) = delete;
+
+  Graph(Graph &&) noexcept = default;
+  Graph &operator=(Graph &&) noexcept = default;
 };
 
 } // namespace ssoc::graph
