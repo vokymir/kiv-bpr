7bde7044c5c2130d265296dcde7ddb0ee3c00a19
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 12:50:33 +0100

    test if is working...

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 
 #include "App.hpp"
+#include "Sim_Config.hpp"
+#include "graph/Generator.hpp"
 int main(void) {
+
+  ssoc::Sim_Config cfg{};
+
+  ssoc::graph::generate::igraph_from_config(cfg);
 
   ssoc::App app{};
 
