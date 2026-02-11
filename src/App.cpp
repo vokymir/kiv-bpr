d97b8b9258bd78ebf6071e6d31135548e544fa36
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 21:00:47 +0100

    app overseeing graph creation

--- src/App.cpp
+++ src/App.cpp
@@ -1,8 +1,20 @@
 #include "App.hpp"
+#include "Sim_Config.hpp"
+#include "Vis_Config.hpp"
+#include "graph/Generator.hpp"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_opengl.h>
 
-void ssoc::App::run() {
+namespace ssoc {
+
+void App::init() {
+  cfg_.gga = gga_::Square_Lattice_2D{3, 4, false, false};
+  cfg_.visual.gla = gla_::Fruchterman_Reingold_2D{gla_::High};
+
+  g_ = graph::generate::igraph_from_config(cfg_);
+}
+
+void App::run() {
   ui_.init();
 
   bool should_end = false;
@@ -21,3 +33,5 @@ void ssoc::App::run() {
 
   ui_.clear();
 }
+
+} // namespace ssoc
--- src/App.hpp
+++ src/App.hpp
@@ -9,12 +9,15 @@ namespace ssoc {
 /* Orchestrates everything. */
 class App {
 private:
-  Sim_Config cfg{};
+  Sim_Config cfg_{};
   std::unique_ptr<graph::Graph> g_{};
   ui::UI ui_{};
 
 public:
   void run();
+
+private:
+  void init();
 };
 
 } // namespace ssoc
