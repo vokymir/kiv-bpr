2e429d069fbbeae4cd0f0d038f6ab8e35fe17479
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 12:50:07 +0100

    igraph can generate only static lattice to test

--- src/graph/Generator_igraph.cpp
+++ src/graph/Generator_igraph.cpp
@@ -1,7 +1,8 @@
 #include "Converter.hpp"
 #include "Generator.hpp"
-#include "igraph_lib.hpp"
+#include "igraph.h"
 #include <memory>
+#include <stdexcept>
 
 namespace ssoc::graph::generate {
 
@@ -11,6 +12,22 @@ generate_igraph(const ssoc::Sim_Config &cfg) {
 
   igraph_::igraph_Deleter deleter;
   igraph_::unique_ptr_ ig(new igraph_t, deleter);
+
+  // create dimension vector
+  igraph_vector_int_t dim;
+  igraph_vector_int_init(&dim, 2); // 2 dimensions
+  VECTOR(dim)[0] = 4;              // rows
+  VECTOR(dim)[1] = 6;              // columns
+
+  // generate square lattice
+  auto success =
+      igraph_square_lattice(ig.get(), &dim, 4, false, false, nullptr);
+
+  igraph_vector_int_destroy(&dim); // free dim
+
+  if (success != IGRAPH_SUCCESS) {
+    throw std::runtime_error("Failed to generate square lattice");
+  }
 
   return ig;
 }
