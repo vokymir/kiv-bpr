0195cb45d06991fe3337281f1924e4e0f89b51ef
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-02-11 12:04:53 +0100

    coverter have igraph deleter

--- src/graph/Converter.hpp
+++ src/graph/Converter.hpp
@@ -1,22 +1,28 @@
 #pragma once
 
 #include "Graph.hpp"
-#include "igraph_datatype.h"
+#include "igraph.h"
 #include <memory>
 namespace ssoc::graph::convert {
 
 /* Adding support for new converters:
  * add two functions:
  * std::unique_ptr<Graph> to_ssoc_graph(const TYPE&library_name);
- * std::unique_ptr<TYPE> to_library_name(const Graph&ssoc_graph);
+ * std::unique_ptr<TYPE[, custom_deleter]> to_library_name(const
+ * Graph&ssoc_graph);
  * */
 
 // =====
 // Library: igraph
 // =====
 
+// custor raii deleter to allow unique_ptr
+struct igraph_Deleter {
+  void operator()(igraph_t *g) const;
+};
+
 std::unique_ptr<Graph> to_ssoc_graph(const igraph_t &igraph);
 
-std::unique_ptr<igraph_t> to_igraph(const Graph &ssoc_graph);
+std::unique_ptr<igraph_t, igraph_Deleter> to_igraph(const Graph &ssoc_graph);
 
 } // namespace ssoc::graph::convert
--- src/graph/Converter_igraph.cpp
+++ src/graph/Converter_igraph.cpp
@@ -2,8 +2,15 @@
 
 namespace ssoc::graph::convert {
 
+void igraph_Deleter::operator()(igraph_t *g) const {
+  if (g) {
+    igraph_destroy(g);
+    delete g;
+  }
+}
+
 std::unique_ptr<Graph> to_ssoc_graph(const igraph_t &igraph) {}
 
-igraph_t to_igraph(const Graph &ssoc_graph) {}
+std::unique_ptr<igraph_t, igraph_Deleter> to_igraph(const Graph &ssoc_graph) {}
 
 } // namespace ssoc::graph::convert
